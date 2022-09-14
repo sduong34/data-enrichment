639dcada96437616e37b276102f5316a7f0e50c1
sduong34 <sarahduong34@gmail.com>
2022-09-13 22:30:29 -0500

    2

--- enrich.py
+++ enrich.py
@@ -18,3 +18,21 @@ neighborhoodatlas.columns = neighborhoodatlas.columns.str.replace('[^A-Za-z0-9]+
 #view types 
 sparcs.dtypes
 neighborhoodatlas.dtypes
+
+#choose which columns to merge 
+sparcs_small = sparcs[['Zip_Code_3_digits','Gender','Length_of_Stay','Total_Costs']]
+print(sparcs_small.sample(10).to_markdown())
+neighborhoodatlas_small = neighborhoodatlas[['ZIPID']]
+print(neighborhoodatlas_small.sample(10).to_markdown())
+
+#combine into one dataset
+combined_df = combined_df = sparcs_small.merge(neighborhoodatlas_small, how='left', left_on='Zip_Code_3_digits', right_on='ZIPID')
+combined_df = combined_df.drop(columns=['ZIPID'])
+combined_df.columns
+
+combined_df_nodups = combined_df.drop_duplicates(subset=['Zip_Code_3_digits'])
+combined_df.head
+
+#save to new csv
+combined_df.to_csv('combined.csv')
+
